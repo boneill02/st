733f26a6146a9ea45f448b699d1fa1d84844387b
Ben O'Neill <ben@benoneill.xyz>
2019-10-27 12:41:18 -0400

    Better form

--- config.def.h
+++ config.def.h
@@ -5,8 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "Inconsolata for Powerline:pixelsize=16:antialias=true:autohint=true" };
+static char *font = "monospace:pixelsize=16:antialias=true:autohint=true";
+static char *font2[] = {
+	"Inconsolata For Powerline:pixelsize=16:antialias=true:autohint=true"
+};
 static int borderpx = 2;
 
 /*
