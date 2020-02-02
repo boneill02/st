eaac284631fc59a63692356174f677389751921a
Ben O'Neill <ben@benoneill.xyz>
2020-02-02 12:13:37 -0500

    fonts

--- config.def.h
+++ config.def.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Noto Sans Mono:pixelsize=16:antialias=true:autohint=true";
 static char *font2[] = {
-	"Inconsolata For Powerline:pixelsize=16:antialias=true:autohint=true"
+	"monospace:pixelsize=16:antialias=true:autohint=true",
 };
 static int borderpx = 2;
 
